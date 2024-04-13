e145f06e98a914970aff98ae898d0001a38ebe72
Piotr Esden-Tempski <piotr@esden.net>
2024-04-13 14:26:09 -0700

    cs-data: Deserialize order and ship dates to NaiveDateTime

--- cs-data/src/order_data.rs
+++ cs-data/src/order_data.rs
@@ -1,5 +1,26 @@
 use std::error::Error;
 use serde::Deserialize;
+use chrono::NaiveDateTime;
+
+mod date_deserializer {
+    use serde::{de::Error, Deserializer};
+    use super::*;
+
+    pub fn deserialize<'de, D: Deserializer<'de>>(deserializer: D) -> Result<NaiveDateTime, D::Error> {
+        let time: String = Deserialize::deserialize(deserializer)?;
+        Ok(NaiveDateTime::parse_from_str(&time, "%Y-%m-%d %H:%M:%S%.f").map_err(D::Error::custom)?)
+    }
+
+    pub fn opt_deserialize<'de, D: Deserializer<'de>>(deserializer: D) -> Result<Option<NaiveDateTime>, D::Error> {
+        let time: String = Deserialize::deserialize(deserializer)?;
+        if time.is_empty() {
+            Ok(None)
+        } else {
+            Ok(Some(NaiveDateTime::parse_from_str(&time, "%Y-%m-%d %H:%M:%S%.f").map_err(D::Error::custom)?))
+        }
+    }
+
+}
 
 /// Crowd Supply order data record
 #[derive(Debug, Deserialize)]
@@ -13,14 +34,18 @@ pub(crate) struct Record {
     pub product_name: String,
     #[serde(rename = "Qty")]
     pub qty: usize,
+    #[serde(rename = "Unit Price")]
+    pub unit_price: f32,
+    #[serde(rename = "Subtotal")]
+    pub subtotal: f32,
     #[serde(rename = "Company")]
     pub company: Option<String>,
     #[serde(rename = "Country Code")]
     pub country_code: String,
-    #[serde(rename = "Placed Time")]
-    pub placed_time: String,
-    #[serde(rename = "Shipped Time")]
-    pub shipped_time: Option<String>,
+    #[serde(rename = "Placed Time", deserialize_with = "date_deserializer::deserialize")]
+    pub placed_time: NaiveDateTime,
+    #[serde(rename = "Shipped Time", deserialize_with = "date_deserializer::opt_deserialize")]
+    pub shipped_time: Option<NaiveDateTime>,
     #[serde(rename = "Tracking")]
     pub tracking: Option<String>
 }
